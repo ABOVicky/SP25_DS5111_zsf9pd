cfb2f835f0a1c3e690e226d9bd1f275d0d784fae
ABOVicky <156977830+ABOVicky@users.noreply.github.com>
2025-02-04 23:05:11 -0500

    Update 00_01_setup_git_global_creds.sh

--- scripts /00_01_setup_git_global_creds.sh
+++ scripts /00_01_setup_git_global_creds.sh
@@ -1,1 +1,11 @@
-s
+!#/usr/bin/bash
+
+USER=TajwinderSK@outlook.com
+NAME=VickySINGHUVA
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
