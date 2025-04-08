be70942b7520964512314ee74c17cdb14e5f2c5b
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-04-08 15:54:33 -0700

    Update test_system.py

--- tests/test_system.py
+++ tests/test_system.py
@@ -10,7 +10,7 @@ def test_os_is_linux():
     """Ensure the tests are running on a Linux machine"""
     assert platform.system() == "Linux", f"Tests must be run on Linux, found: {platform.system()}"
 
-def test_python_version():
+def test_python_version(): # head's up that this will have to change after updating the validations.yaml github action file
     """Ensure the Python version is 3.10 or 3.11"""
     major, minor = sys.version_info[:2]
     assert (major, minor) in [(3, 10), (3, 11), (3, 12)], f"Python version must be 3.10 -  3.12, found: {major}.{minor}"
